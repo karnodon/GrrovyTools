242a89addee8d9e36a3d856ba7fd4efcfc62be56
karnodon <frostbite3@gmail.com>
2013-01-08 21:28:00 +0400

    mp3 list

--- src/MP3List.groovy
+++ src/MP3List.groovy
@@ -3,109 +3,93 @@
  * User: Frostbite
  * Date: 05.01.13
  * Time: 20:04
- * To change this template use File | Settings | File Templates.
  */
 import static groovy.io.FileType.FILES
+
 class MP3List {
-    TreeMap foundFiles;
+    ArrayList foundFiles;
     File root;
     PrintStream fw;
     boolean showFiles;
     int totalAlbums;
+    int totalFiles;
 
-    private boolean walk(File directory, TreeMap store) {
-        boolean foundAnything = false;
-        if (directory.isDirectory()) {
-            ArrayList files = new ArrayList();
-            ArrayList<File> dirs = new ArrayList<File>();
-            directory.eachFile() {
-                if(it.name.endsWith('.mp3')  || it.name.endsWith(".ogg") || it.name.endsWith(".wma")) {
-                    files.add(it)
-                }
-                if (it.directory) {
-                    dirs.add(it);
-                }
-            };
-            ArrayList data = new ArrayList(2);
-            store.put(directory, data);
-            if (files != null) {
-                if (files.size() > 0) {
-                    data.add(files);
-                    totalAlbums++;
-                    foundAnything = true;
-                }
-                if (!dirs.isEmpty()) {
-                    TreeMap<File, ArrayList> sub = new TreeMap<File,ArrayList>();
-                    data.add(sub);
-                    for (dir in dirs) {
-                        if (!walk(dir, sub)) {
-                            sub.remove(dir);
-                        }
-                        else {
-                            foundAnything = true;
-                        }
-                    }
-                    sub.sort() {a,b -> a.key.getAbsolutePath() <=> b.key.getAbsolutePath()};
-                }
+    def walk(root, fileList) {
+        ArrayList files = new ArrayList();
+        ArrayList dirs = new ArrayList();
+        root.eachFile() {
+            if (it.name.endsWith('.mp3') || it.name.endsWith(".ogg") || it.name.endsWith(".wma")) {
+                files.add(it.getAbsolutePath())
             }
-            else {
-                println(directory.getAbsolutePath());
+            if (it.directory) {
+                dirs.add(it);
+            }
+        };
+        if (files.size() > 0 || dirs.size() > 0) {
+            fileList.add(root.getAbsolutePath());
+            fileList.add(files);
+            totalFiles += files.size();
+            if (dirs.isEmpty()) {
+                totalAlbums++;
+            }
+            for (d in dirs) {
+                ArrayList subList = new ArrayList();
+                fileList.add(subList);
+                walk(d, subList);
             }
         }
-        return foundAnything;
     }
 
-    private void print(TreeMap result) {
-        fw.println("<UL>");
-        for (Iterator iterator = result.keySet().iterator(); iterator.hasNext();) {
-            File key = (File) iterator.next();
-            ArrayList o = (ArrayList) result.get(key);
-            fw.println("<LI>" + key + "</LI>");
-
-            for (int i = 0; i < o.size(); i++) {
-                Object o1 = o.get(i);
-                if (o1 instanceof ArrayList && showFiles) {
-                    fw.print("<UL>");
-                    for (path in o1) {
-                        fw.println("<LI>" + path + "</LI>");
-                    }
-                    fw.println("</UL>");
-                } else if (o1 instanceof TreeMap) {
-                    print((TreeMap) o1);
+    def print(tgt, htmlFile) {
+        if (!tgt.isEmpty()) {
+            htmlFile.print(("<li>" + tgt[0] + "</li><ul>"))
+            if (showFiles)
+                try {
+                    def files = tgt[1]
+                    for (f in files)
+                        htmlFile.print("<li>" + f + "</li>")
+                }
+                catch (Exception e) {}
+            try {
+                for (int i = 2; i < tgt.size(); i++) {
+                    print(tgt[i], htmlFile)
                 }
             }
+            catch (Exception e) {}
+            htmlFile.print("</ul>")
         }
-        fw.println("</UL>");
     }
 
-    public MP3List(boolean showFiles) {
+    public MP3List(boolean showFiles, String root, String htmlPath) {
         this.showFiles = showFiles;
         totalAlbums = 0;
-        foundFiles = new TreeMap(new Comparator() {
-            public int compare(Object o1, Object o2) {
-                File f1 = (File) o1;
-                File f2 = (File) o2;
-                return f1.getAbsolutePath().compareTo(f2.getAbsolutePath());  //To change body of implemented methods use File | Settings | File Templates.
-            }
-        });
+        totalFiles = 0;
+        foundFiles = new ArrayList()
+        long start = System.currentTimeMillis()
         try {
-            fw = new PrintStream(new FileOutputStream("c:/Users/Frostbite/Documents/result.html"));
+            fw = new PrintStream(new FileOutputStream(htmlPath));
+            walk(new File(root), foundFiles)
+            fw.print("<html><body><ul>")
+            print(foundFiles, fw)
+            fw.print("</ul><ul><li>всего альбомов: " + totalAlbums + "</li>")
+            fw.print("<li>всего записей: " +totalFiles + "</li></ul>")
+            fw.print("</body></html>")
+            start =  (System.currentTimeMillis() - start)/1000
+            println "Выполнено за " + start + "с"
         }
         catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-        boolean b = false;
-        if (args.length > 1)
-            b = "Y".equalsIgnoreCase(args[1]);
-        MP3List l = new MP3List(b);
-        l.walk(new File(args[0]), l.foundFiles);
-        l.fw.print("<HTML><BODY>");
-        l.fw.print("<H1>Total albums: " + l.totalAlbums + "</H1>");
-        l.print(l.foundFiles);
-        l.fw.print("</BODY></HTML>");
-        l.fw.flush();
+        if (args.length < 2) {
+            println "Usage: java MP3List [-f] <root> <target>"
+        } else {
+            int argIdx = 0
+            if (args[0] == "-f")
+                argIdx = 1
+            new MP3List(args[0] == "-f", args[argIdx], args[argIdx + 1])
+        }
     }
 }
